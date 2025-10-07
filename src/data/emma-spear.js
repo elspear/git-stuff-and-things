dc4e4e999958784404fa2d3c27a275ffe62e1746
Emma Spear <emmspear@gmail.com>
2025-10-07 19:47:59 +0800

    fix: picture for emma-spear.js

---
+++ src/data/emma-spear.js
@@ -0,0 +1,12 @@
+// this is a template to follow
+// for the data object.
+
+const data = {
+  name: "Emma Spear",
+  caption: "git master",
+  coverImageUrl: "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
+  bio: `this is my bio! It is a very good bio, the best bio if you ask me`,
+};
+
+
+export default data;
