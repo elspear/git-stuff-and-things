96e7669cdbf99915f0d88a2c16d816032b9ab54e
Jess-K92 <jkeating@live.com.au>
2025-10-07 18:40:21 +0800

    feat: create jess user template

---
+++ src/data/jessica-keating.js
@@ -0,0 +1,11 @@
+// this is a template to follow
+// for the data object.
+
+const data = {
+  name: "Jessica Keating",
+  caption: "loves to She Codes!",
+  coverImageUrl: "https://jpittard.net/mortalwombat.jpg",
+  bio: ` Loves dogs, pilates, coffee and coding.`,
+};
+
+export default data;
